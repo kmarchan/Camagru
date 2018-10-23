921bd7e26be304b45c2c87cd7b1804c11488f62a
kmarchan <kmarchan@student.wethinkcode.co.za>
2018-10-23 13:25:54 +0200

    register

--- server.php
+++ server.php
@@ -132,4 +132,36 @@
 
         }
     }
+    // if (isset($_POST['register']))
+    // {
+    //     $email = ($_POST['email']);
+    //     // console.log($username);
+    //     if(empty($email))
+    //     {
+    //         array_push($errors, "Email is required");
+    //     }
+    //     if(count($errors) == 0)
+    //     {
+    //         $password = hash("whirlpool", $password_1);
+    //         $confirmcode = rand();
+    //         $sql = "INSERT INTO users (username, name, surname, email, password, confirmcode) VALUES ('$username', '$name', '$surname', '$email', '$password', '$confirmcode')";
+    //         $db->exec($sql);
+    //         $headers = "From: noreply@localhost.co.za\r\n";
+    //         $headers .= "Reply-To: noreply@localhost.co.za\r\n";
+    //         $headers .= "Return-Path: noreply@localhost.co.za\r\n";
+    //         $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+    //         $message = "<h1>Activate Your Account.</h1>
+    //         Click the link below to Verify your account <br />
+    //         <br />
+    //             For <strong>Localhost Activation </strong> use the following : <br />
+    //                 http://localhost:8080/Camagru/email_confirm.php?username=$username&code=$confirmcode <br />
+    //         <h2>Enjoy</h2>
+    //         ";
+    //         mail($email, "Activation email", $message , $headers);
+    //         $login_message = "Check Your Email for the Activation link";
+    //         $_SESSION['message'] = $login_message;
+    //         header('Location: index.php');
+    //     }
+    // }
+
 ?>
